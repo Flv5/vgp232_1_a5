e9f319cba4b782beb6b481730c43254305df1cd8
floaiza40 <floaiza40@gmail.com>
2018-05-31 16:51:18 -0700

    Changes
    No clue

--- Character/Character.cs
+++ Character/Character.cs
@@ -4,32 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Character
+namespace Character 
 {
-    public enum Race { Dwarf, Elf, Goblin, Giant, Halfling, Orc, Vampires, Werewolf, Sphinx }
-    public enum Alignment { LawfulGood, LawfulNeutral, LawfulEvil, NeutralGood, Neutral, NeutralEvil, ChaoticGood, ChaoticNeutral, ChaoticEvil }
+    public enum Races { Dwarf, Elf, Goblin, Giant, Halfling, Orc, Vampires, Werewolf, Sphinx }
+    public enum Alignments { LawfulGood, LawfulNeutral, LawfulEvil, NeutralGood, Neutral, NeutralEvil, ChaoticGood, ChaoticNeutral, ChaoticEvil }
 
     public class Character
     {
+        String mName;
+        String mGender; 
         int healthPoints;
-        Race race;
-        Alignment alignment;
-
-        public Character(int hp, Race r, Alignment a)
+        Races race;
+        Alignments alignment;
+        public Character(int hp, Races r, Alignments a, String name, String gender)
         {
+            mName = name;
+            mGender = gender;
             healthPoints = hp;
             race = r;
             alignment = a;
         }
+        public int HealthPoints
+        {
+            get; set;
+        }
+
+        public Races Race
+        {
+            get; set;
+        }
+
+        public Alignments Alignment
+        {
+            get; set;
+        }
+
+        public Character(int hp, Races r, Alignments a)
+        {
+            HealthPoints = hp;
+            Race = r;
+            Alignment = a;
+        }
 
         public void TakeDamage(int damage)
         {
-            healthPoints -= 1;
+            healthPoints -= damage;
         }
 
         public void RestoreHealth(int amount)
         {
-            healthPoints += 1;
+            healthPoints += amount;
         }
     }
 }
