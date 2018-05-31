853b5118a918d21178ccf7a779ca5afaf8c233ef
floaiza40 <floaiza40@gmail.com>
2018-05-31 16:16:41 -0700

    Modified
    -Fixing bugs
    -Adding properties to the class Character

--- Character/Character.cs
+++ Character/Character.cs
@@ -4,19 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Character
+namespace Character 
 {
     public enum Race { Dwarf, Elf, Goblin, Giant, Halfling, Orc, Vampires, Werewolf, Sphinx }
     public enum Alignment { LawfulGood, LawfulNeutral, LawfulEvil, NeutralGood, Neutral, NeutralEvil, ChaoticGood, ChaoticNeutral, ChaoticEvil }
 
     public class Character
     {
+        String mName;
+        String mGender; 
         int healthPoints;
         Race race;
         Alignment alignment;
-
-        public Character(int hp, Race r, Alignment a)
+        public Character(int hp, Race r, Alignment a, String name, String gender)
         {
+            mName = name;
+            mGender = gender;
             healthPoints = hp;
             race = r;
             alignment = a;
@@ -24,12 +27,12 @@ namespace Character
 
         public void TakeDamage(int damage)
         {
-            healthPoints -= 1;
+            healthPoints -= damage;
         }
 
         public void RestoreHealth(int amount)
         {
-            healthPoints += 1;
+            healthPoints += amount;
         }
     }
 }
--- Character/CharacterManager.cs
+++ Character/CharacterManager.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-
 namespace Character
 {
     public class CharacterManager
