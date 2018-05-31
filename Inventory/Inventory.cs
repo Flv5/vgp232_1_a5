9970d6592260a3d01ad19a1f730bb68abd215aaa
floaiza40 <floaiza40@gmail.com>
2018-05-31 15:35:35 -0700

    Completing_Inventory

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -6,24 +6,58 @@ using System.Threading.Tasks;
 
 namespace Inventory
 {
+    public enum Weapon { Knife, Bow, Axe, Sword, Wand }
+    public enum Potion { Health_Full, Health_Standar, Magic_Full, Magic_Standar }
+
     public class Inventory
     {
-        int slots;
-        List<Item> items;
-        
-        public Inventory(int slots)
+        List<Weapon> mWeapons;
+        List<Potion> mPotions;
+        int mPotionIndex;
+        int mWeaponIndex;
+
+
+        public Inventory(int maxWeaponCount, int maxPotionCount)
         {
-            items = new List<Item>(slots);
+            mWeapons = new List<Weapon>(maxWeaponCount);
+            mPotions = new List<Potion>(maxPotionCount);
+
+            mPotionIndex = 0;
+            mWeaponIndex = 0;
         }
 
-        public void Add(Item item)
+        void InsertPotion(Potion potion)
         {
-            items.Add(item);
+            if (mPotions.Count < mPotions.Capacity)
+            {
+                mPotions.Insert(mPotionIndex, potion);
+                ++mPotionIndex;
+            }
+            else
+                Console.WriteLine("Not enough space for potions");
         }
 
-        public void Remove(Item item)
+        void InsertWeapon(Weapon weapon)
         {
-            items.Remove(item);
+            if (mWeapons.Count < mWeapons.Capacity)
+            {
+                mWeapons.Insert(mWeaponIndex, weapon);
+                ++mWeaponIndex;
+            }
+            else
+                Console.WriteLine("Not enough space for weapons");
+        }
+
+        void RemovePotion(Potion potion)
+        {
+            mPotions.Remove(potion);
+            --mPotionIndex;
+        }
+
+        void RemoveWeapon(Weapon weapon)
+        {
+            mWeapons.Remove(weapon);
+            --mWeaponIndex;
         }
     }
 }
