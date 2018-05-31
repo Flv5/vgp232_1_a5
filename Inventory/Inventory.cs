f500d0246753b8b4194b710813029cd4aee23755
floaiza40 <floaiza40@gmail.com>
2018-05-31 15:27:09 -0700

    Filling Inventory
    [VGP 232]
    
    -Giving properties and methos to Inventory

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Inventory
 {
+    public enum Weapon { Knife, Bow, Axe, Sword, Wand }
+    public enum Potion { Health_Full, Health_Standar, Magic_Full, Magic_Standar }
+
     public class Inventory
     {
+        List<Weapon> mWeapons;
+        List<Potion> mPotions;
+        int mPotionIndex;
+        int mWeaponIndex;
+
+        int mMaxWeaponCount;
+        int mMaxPotionCount;
+
+        public Inventory(int maxWeaponCount, int maxPotionCount)
+        {
+            mWeapons.Capacity = maxWeaponCount;
+            mPotions.Capacity = maxPotionCount;
+
+            mPotionIndex = 0;
+            mWeaponIndex = 0;
+        }
+
+        void InsertPotion(Potion potion)
+        {
+            if (mPotions.Capacity < mMaxPotionCount)
+            {
+                mPotions.Insert(mPotionIndex, potion);
+                ++mPotionIndex;
+            }
+            else
+                Console.WriteLine("Not enough space for potions");
+        }
+
+        void InsertWeapon(Weapon weapon)
+        {
+            if (mWeapons.Capacity < mMaxWeaponCount)
+            {
+                mWeapons.Insert(mWeaponIndex, weapon);
+                ++mWeaponIndex;
+            }
+            else
+                Console.WriteLine("Not enough space for weapons");
+        }
     }
 }
